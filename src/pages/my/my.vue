2baa1eabb6ffa890f377ae995bee21979c040e5b
“流霜归魄” <1608936228@qq.com>
2023-11-09 15:59:47 +0800

    上传

---
+++ src/pages/my/my.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+</script>
+
+<template>
+  <view class="my">
+    <view>用户信息：{{ memberStore.profile }}</view>
+    <button @tap="memberStore.setProfile({ nickname: '王東' })" size="mini" plain type="primary">
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+
+    <uni-section title="设置" type="line">
+      <uni-list>
+        <uni-list-item showArrow title="去登录" :to="`/pages/login/login`" />
+      </uni-list>
+    </uni-section>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
